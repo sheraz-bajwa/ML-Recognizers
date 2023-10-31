3e9e40482a56d9a8baf15a2cd9f89e25ac87acd7
sheraz-bajwa <sheezybajwa@gmail.com>
2023-10-31 10:20:54 +0500

    first commit

---
+++ fyp/lib/home.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'dart:io';
+
+class TextRecognitionExample extends StatefulWidget {
+  @override
+  _TextRecognitionExampleState createState() => _TextRecognitionExampleState();
+}
+
+class _TextRecognitionExampleState extends State<TextRecognitionExample> {
+  String scannedText = '';
+  bool isTextScanning = false;
+
+  Future<void> pickAndRecognizeText() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      final imageFile = File(pickedImage.path);
+      await recognizeText(imageFile);
+    }
+  }
+
+  Future<void> recognizeText(File imageFile) async {
+    setState(() {
+      isTextScanning = true;
+    });
+
+    final inputImage = InputImage.fromFile(imageFile);
+    final textRecognizer = GoogleMlKit.vision.textRecognizer();
+    RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
+
+    scannedText = '';
+
+    for (TextBlock block in recognizedText.blocks) {
+      for (TextLine line in block.lines) {
+        scannedText += line.text + '\n';
+      }
+    }
+
+    await textRecognizer.close();
+
+    setState(() {
+      isTextScanning = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Text Recognition Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            
+            if (scannedText.isNotEmpty)
+              Text(scannedText)
+            else
+              Text('No text scanned'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: pickAndRecognizeText,
+              child: Text('Pick Image and Recognize Text'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ fyp/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:fyp/home.dart';
+import 'package:google_ml_vision/google_ml_vision.dart';
+// import 'package:google_ml_kit/google_ml_kit.dart';
+// import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: TextRecognitionExample(),
+    );
+  }
+}
